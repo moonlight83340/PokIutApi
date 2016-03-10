235694bea98ae340b4a15477b2385f15de137bd7
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-10 18:14:58 +0100

    add framework to API

---
+++ PokeAPi/App/Views/Home/index.html.php
@@ -0,0 +1,1 @@
+<?php $this->setLayout('layout', ['title' => 'PokIutApi v1.0 - Accueil', 'keywords' => 'PokIutApi, accueil, pokemon,api,iut,aix en provence,aix', 'description' => '']); ?>
---
+++ PokeAPi/App/Views/layout.html.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title><?= htmlentities($title)?></title>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name = " author " content = "Gaëtan Perrot" >
+        <meta name = " keywords " content = "<?= htmlentities($keywords) ?>">
+        <meta name = " description " content = "<?= htmlentities($description)?>">
+        <link href="/Resources/Bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="icon" href="/Resources/Image/Favicon/favicon.ico" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="/Resources/Bootstrap/js/bootstrap.js"></script>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>  
+        <?= $contents?>
+    </body>
+</html>
