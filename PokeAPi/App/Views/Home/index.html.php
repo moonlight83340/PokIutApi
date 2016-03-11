d9741424bd0c0023263e0ac582e95c3c7260a7cf
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-11 20:18:16 +0100

    delete jumbontron

--- PokeAPi/App/Views/Home/index.html.php
+++ PokeAPi/App/Views/Home/index.html.php
@@ -1,8 +1,34 @@
 <?php $this->setLayout('layout', ['title' => 'Pokiut Api v1.0 - Accueil', 'keywords' => 'PokIutApi, accueil, pokemon,api,iut,aix en provence,aix', 'description' => '']); ?>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+          <a class="navbar-brand" href="<?= \PoireauFramework\Helper\Url::base()?>">Pokiut Api</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li> <a href="#about"> About </a> </li>
+          <li> <a href="#Pokedex"> Pokedex </a> </li>
+        </ul>
+      </div><!--/.navbar-collapse -->
+    </div>
+</nav>
 <div class="jumbotron">
     <div class="container">
-      <h1>Hello, world!</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+        <h1>Pokiut Api <small>La première API pokémon en français !</small></h1>
     </div>
 </div>
+<div class="container marketing">
+    <div class="row">
+        <div id="about"><h2>À propos</h2></div>
+        <hr>
+    </div>
+</div>
+<footer>
+    <p class="text-center"><a href="#">Haut de page </a> ©  Pokiut Api 2016</p>
+</footer>
